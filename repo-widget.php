c76cb6e243d1f3d2abcba6f44d28467b25239c59
allejo <allejo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-24 06:50:41 +0000

    Finished added parameters to sprintf statements and added JSON information to be stored in a transient
    git-svn-id: https://plugins.svn.wordpress.org/repo-widget/trunk@809495 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- repo-widget.php
+++ repo-widget.php
@@ -18,6 +18,15 @@ function repo_widget_handler($attributes)
     wp_register_script('repo-widget-js', plugins_url('repo-widget.js', __FILE__ ), array('jquery'));
     wp_enqueue_script('repo-widget-js');
 
+    $repo_widget = repo_widget_builder($attributes);
+
+    return $repo_widget;
+}
+
+function repo_widget_builder($attributes)
+{
+    $widget = "";
+
     extract(shortcode_atts(array(
         'host' => 'github',
         'user' => 'octocat',
@@ -28,96 +37,87 @@ function repo_widget_handler($attributes)
         'travis' => ''
     ), $attributes));
 
-    $repo_widget = repo_widget_builder($attributes);
+    if ($theme == "github")
+    {
+        if ($size == "normal")
+        {
+            $data = repo_widget_json("github", array('user' => $user, 'repo' => $repo));
 
-    return $repo_widget;
+            $widget =
+                sprintf('<div class="repo-widget github normal">') .
+                sprintf('    <div class="header">') .
+                sprintf('        <a href="%s">%s</a>', $data['url'], $data['name']);
+
+            if ($travis != '')
+            {
+                $widget .= sprintf('        <img src="%s">', $travis);
+            }
+
+            $widget .=
+                sprintf('    </div>') .
+                sprintf('    <div class="information">') .
+                sprintf('        <div class="links">') .
+                sprintf('            <div class="active" rel="%s">HTTP</div>', $data['clone_url']) .
+                sprintf('            <div rel="%s">GIT</div>', $data['git_url']) .
+                sprintf('            <div rel="%s">SSH</div>', $data['ssh_url']) .
+                sprintf('            <input value="%s" readonly onclick="this.select()">', $data['clone_url']) .
+                sprintf('        </div>') .
+                sprintf('        <div class="description">') .
+                sprintf('            <p>%s</p>', $data['description']) .
+                sprintf('        </div>') .
+                sprintf('    </div>') .
+                sprintf('    <ul class="commits">') .
+                sprintf('        <li>') .
+                sprintf('            <div class="info">') .
+                sprintf('                <p class="commit">latest commit <a href="">%s</a></p>', $data['last_commit']['hash']) .
+                sprintf('                <p class="timestamp">%s</p>', $data['last_commit']['date']) .
+                sprintf('                <div style="clear:both"></div>') .
+                sprintf('            </div>') .
+                sprintf('            <p class="message">%s by <em>%s</em></p>', $data['last_commit']['message'], $data['last_commit']['author']) .
+                sprintf('        </li>') .
+                sprintf('    </ul>') .
+                sprintf('</div>');
+            }
+    }
+
+    return $widget;
 }
 
-function repo_widget_builder($attributes)
+function repo_widget_json($host, $array)
 {
+    $transient = "repo-widget_" . $array['user'] . "-" . $array['repo'];
+	$status = get_transient($transient);
 
-    return "foobar";
+    if ($status)
+    {
+        return $status;
+    }
+
+    if ($host == "github")
+    {
+        $repo_location = $array['user'] . '/' . $array['repo'];
+
+        $repo_data  = json_decode(file_get_contents("https://api.github.com/repos/" . $repo_location), TRUE);
+        $commit_sha = json_decode(file_get_contents("https://api.github.com/repos/" . $repo_location . "/git/refs/heads/master"), TRUE);
+        $commit     = json_decode(file_get_contents("https://api.github.com/repos/" . $repo_location . "/git/commits/" . $commit_sha['object']['sha']), TRUE);
+        $date       = new DateTime($commit['author']['date']);
+
+        $repo_information = array();
+        $repo_information['name'] = $repo_data['name'];
+        $repo_information['url'] = $repo_data['html_url'];
+        $repo_information['clone_url'] = $repo_data['clone_url'];
+        $repo_information['git_url'] = $repo_data['git_url'];
+        $repo_information['ssh_url'] = $repo_data['ssh_url'];
+        $repo_information['description'] = $repo_data['description'];
+        $repo_information['last_commit']['hash'] = substr($commit_sha['object']['sha'], 0, 10);
+        $repo_information['last_commit']['url'] = $commit['html_url'];
+        $repo_information['last_commit']['date'] = $date->format("D M j G:i:s Y");
+        $repo_information['last_commit']['message'] = $commit['message'];
+        $repo_information['last_commit']['author'] = $commit['author']['name'];
+
+        set_transient($transient, $repo_information, 90);
+        return $repo_information;
+    }
 }
 
 add_shortcode('repo', 'repo_widget_handler');
-
-/*function repo_tag($atts)
-{
-	wp_register_style('repo-widget-css', plugins_url('style.css', __FILE__ ));
-	wp_enqueue_style('repo-widget-css');
-	wp_register_script('repo-widget-js', plugins_url('repo-widget.js', __FILE__ ), array('jquery'));
-	wp_enqueue_script('repo-widget-js');
-
-    var_dump($atts);
-
-	extract( shortcode_atts( array(
-		'user' => 'octocat',
-		'repo' => 'Hello-World'
-	), $atts ) );
-
-	$transient = "rw_allejo_los";
-	/*$status = get_transient($transient);
-
-	if ($status == true)
-	{
-		return $status;
-	}
-	else if (/*!empty($user) && !empty($repo))
-	{
-		$repo_location = $user . "/" . $repo; //GitHub api -> user/repo
-
-	    //$repo_data = json_decode(file_get_contents("https://api.github.com/repos/" . $repo_location), TRUE);
-	    //$commit_sha = json_decode(file_get_contents("https://api.github.com/repos/" . $repo_location . "/git/refs/heads/master"), TRUE);
-	    //$commit = json_decode(file_get_contents("https://api.github.com/repos/" . $repo_location . "/git/commits/" . $commit_sha['object']['sha']), TRUE);
-	    //$date = new DateTime($commit['author']['date']);
-
-        //var_dump($repo_data);
-
-	    if (count((array)$repo_data) == 1)
-	    {
-	    	$error_message = "<div class=\"repo_widget\">
-		    <div>
-		        <h1 class=\"rw_title\"><a>Message from GitHub</a></h1>
-		    </div>
-		    <div class=\"rw_description\">
-		    	<br>
-		        <p class=\"rw_p\">" . $commit['message'] . "</p>
-		    </div>
-		</div>";
-	    	return $error_message;
-	    }
-
-		$my_repo_widget = "
-		<div class=\"repo_widget\">
-		    <div>
-		        <h1 class=\"rw_title\"><a href=\"" . $repo_data['html_url'] . "\" target=\"_blank\">" . $repo_data['name'] . "</a></h1>
-		    </div>
-		    <div class=\"rw_link_container\">
-		        <div class=\"rw_links\">
-		            <span class=\"rw_active rw_first rw_button\" rel=\"" . $repo_data['clone_url'] . "\">HTTP</span>
-		            <span class=\"rw_button\" rel=\"" . $repo_data['git_url'] . "\">SSH</span>
-		            <span class=\"rw_button\" rel=\"" . $repo_data['ssh_url'] . "\">GIT Read-Only</span>
-		            <input class=\"rw_input\" value=\"" . $repo_data['clone_url'] . "\" readonly onclick=\"this.select()\">
-		        </div>
-		    </div>
-		    <div class=\"rw_description\">
-		        <p class=\"rw_p\">" . $repo_data['description'] . "</p>
-		    </div>
-		    <div class=\"rw_commit\">
-		        <span class=\"rw_data\">
-		            latest commit <a class=\"rw_hash\" href=\"" . $commit['html_url'] . "\">" . substr($commit['sha'], 0, 10) . "</a>
-		            <span class=\"rw_timestamp\">" . $date->format("D M j G:i:s Y") . "</span>
-		        </span>
-		        <p class=\"rw_message\">" . $commit['message'] . " by <strong>" . $commit['author']['name'] . "</strong></p>
-		    </div>
-		</div>";
-
-		//set_transient($transient, $my_repo_widget, 90);
-		return "hiiiii";
-	}
-	else
-	{
-		return false;
-	}
-}*/
-
